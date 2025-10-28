e7a48001ce59e028d4888ce6082d37285c847c61
MehdiUI <mehdi.lemaire.etu@gmail.com>
2025-10-28 16:17:09 +0100

    affichage btn fav + info produit + affichage produit

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
+import ProductGrid from "@/components/ProductGrid";
 
 
 import React from "react";
@@ -7,12 +8,13 @@ import React from "react";
 const Home = async () => {
 
   return (
-    <Container className="bg-shop-light-pink">
+    <Container>
       <HomeBanner />
-     
+      <ProductGrid />
       
       
     </Container>
+    
   );
 };
 
