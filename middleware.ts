15860453db162fa2a9d5d19e5a281d3add564bbc
MehdiUI <mehdi.lemaire.etu@gmail.com>
2025-11-03 13:12:41 +0100

    bug fix v1

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,9 @@ export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Exclure explicitement le webhook Stripe
+    '/((?!_next|webhook|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes EXCEPT webhook
+    '/(api|trpc)(?!/webhook)(.*)',
   ],
 }
